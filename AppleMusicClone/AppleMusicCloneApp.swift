b646a910364d1f1470556295c28663d7fd60c46d
Shingo TOYODA <toyodasinngo@gmail.com>
2023-11-04 02:14:56 +0900

    Initial Commit

---
+++ AppleMusicClone/AppleMusicCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicCloneApp.swift
+//  AppleMusicClone
+//
+//  Created by Shin Toyo on 2023/11/04.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMusicClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleMusicClone
+//
+//  Created by Shin Toyo on 2023/11/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
